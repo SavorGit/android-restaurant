b3459ef24055f62c0b222be4a804e4ef635a17db
dreamkid <dreamkid0924@163.com>
2017-12-07 18:24:09 +0800

    co

---
+++ app/src/main/java/com/savor/resturant/widget/LoadingDialog.java
@@ -0,0 +1,37 @@
+package com.savor.resturant.widget;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.savor.resturant.R;
+
+/**
+ * 上传投屏图片进度
+ * Created by hezd on 2016/12/26.
+ */
+
+public class LoadingDialog extends Dialog {
+
+
+    private final Activity mContext;
+    private TextView mPercentTv;
+    private TextView mHintTv;
+    private TextView mCancelBtn;
+
+    public LoadingDialog(Activity context) {
+        super(context, R.style.loading_progress_bar);
+        this.mContext = context;
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_loading);
+        setCancelable(false);
+    }
+
+}
