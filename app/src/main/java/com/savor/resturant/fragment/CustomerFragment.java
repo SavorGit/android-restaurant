71370658efaf384f2969656d72dd965b5ecedcb3
dreamkid <dreamkid0924@163.com>
2017-12-22 17:30:41 +0800

    adf

--- app/src/main/java/com/savor/resturant/fragment/CustomerFragment.java
+++ app/src/main/java/com/savor/resturant/fragment/CustomerFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.savor.resturant.R;
 
@@ -15,6 +17,9 @@ import com.savor.resturant.R;
  */
 public class CustomerFragment extends Fragment {
 
+
+    private TextView mTitleTv;
+    private ImageView mRightIv;
 
     public CustomerFragment() {
         // Required empty public constructor
@@ -39,8 +44,23 @@ public class CustomerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer, container, false);
+        View parentLayout = inflater.inflate(R.layout.fragment_customer, container, false);
+        initViews(parentLayout);
+        setViews();
+        return parentLayout;
+    }
+
+    private void setViews() {
+        mTitleTv.setText("客户管理");
+        mTitleTv.setTextColor(getResources().getColor(R.color.white));
+        mRightIv.setVisibility(View.VISIBLE);
+        mRightIv.setImageResource(R.drawable.ico_slide_video);
+    }
+
+    private void initViews(View parentLayout) {
+        parentLayout.findViewById(R.id.iv_left).setVisibility(View.GONE);
+        mTitleTv = (TextView) parentLayout.findViewById(R.id.tv_center);
+        mRightIv = (ImageView) parentLayout.findViewById(R.id.iv_right);
     }
 
 }
