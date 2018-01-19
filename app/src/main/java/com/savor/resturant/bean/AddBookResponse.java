d44bfac088620e21f883b9c544c6390cc1449105
dreamkid <dreamkid0924@163.com>
2018-01-19 13:35:44 +0800

    co

---
+++ app/src/main/java/com/savor/resturant/bean/AddBookResponse.java
@@ -0,0 +1,11 @@
+package com.savor.resturant.bean;
+
+import java.io.Serializable;
+
+/**
+ * 添加预定接口返回
+ * Created by hezd on 2018/1/19.
+ */
+
+public class AddBookResponse implements Serializable {
+}
